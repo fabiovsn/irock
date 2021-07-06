870d169c074f3a3c19497f3c39b33b48040add44
fabiovsn <fabiovsn@hotmail.com>
2021-07-06 12:08:01 -0300

    refatoração

--- cookie.js
+++ cookie.js
@@ -2,7 +2,7 @@ function writeCookie(name, value, days){
     //por padrão não há expiração, então o cookie é temporário
     var expires = "";
 
-// especificar um número de dias torna o cookie persistente
+//especificar um número de dias torna o cookie persistente
     if(days){
         var date = new Date();
         date.setTime(date.getTime() + (days*24*60*60*1000));
@@ -16,13 +16,13 @@ function writeCookie(name, value, days){
 }
 
 function readCookie(name){
-    //Busque o cookie especificado e retorne seu valor
+    //busque o cookie especificado e retorne seu valor
 
     var searchName = name + "=";
-    var cookies = document.cookie.split(";");
+    var cookies = document.cookie.split(';');
     for (var i = 0; i < cookies.length; i++){
         var c = cookies[i]
-        while (c.charAt(0)=='')
+        while (c.charAt(0)==' ')
             c = c.substring(1, c.length);
         if (c.indexOf(searchName)==0)
             return c.substring(searchName.length, c.length);
